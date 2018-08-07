a16232bbcc0809ad54766822c1ce55fba73276da
Drinker0k <dmkazantsev@dev.picom.ru>
2018-08-07 11:11:12 +0400

    Исправление замечаний v0.1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 'use strict';
 
-const KEY_CODES = {
+const KEY_CODE = {
   arrowLeft: 37,
   arrowRight: 39
 };
 const app = document.querySelector(`.app`);
+const appContainer = app.querySelector(`section.main`);
 const screens = Array.from(document.querySelectorAll(`template`)).map((item) => item.content);
 
 const setScreen = (index) => {
@@ -13,8 +14,8 @@ const setScreen = (index) => {
 
   screenCurrent = index;
 
-  app.innerHTML = ``;
-  app.appendChild(screens[screenCurrent].cloneNode(true));
+  appContainer.innerHTML = ``;
+  appContainer.appendChild(screens[screenCurrent].cloneNode(true));
 };
 
 let screenCurrent = 0;
@@ -22,48 +23,47 @@ setScreen(screenCurrent);
 
 document.addEventListener(`keydown`, (evt) => {
   switch (evt.keyCode) {
-    case KEY_CODES.arrowLeft:
+    case KEY_CODE.arrowLeft:
       setScreen(screenCurrent - 1);
       break;
-    case KEY_CODES.arrowRight:
+    case KEY_CODE.arrowRight:
       setScreen(screenCurrent + 1);
       break;
   }
 });
 
 
-// вставка стрелок навигации
-{
-  function renderArrows() {
-    const arrowsDiv = document.createElement(`div`);
-    arrowsDiv.classList.add(`arrows__wrap`);
-    arrowsDiv.innerHTML = `<style>
-      .arrows__wrap {
-        position: absolute;
-        top: 135px;
-        left: 50%;
-        margin-left: -56px;
-      }
-      .arrows__btn {
-        background: none;
-        border: 2px solid black;
-        padding: 5px 20px;
-      }
-    </style>
-    <button class="arrows__btn"><-</button>
-    <button class="arrows__btn">-></button>`;
+function renderArrows() {
+  const arrowsDiv = document.createElement(`div`);
+  arrowsDiv.classList.add(`arrows__wrap`);
+  arrowsDiv.innerHTML = `<style>
+    .arrows__wrap {
+      position: absolute;
+      top: 135px;
+      left: 50%;
+      margin-left: -56px;
+    }
+    .arrows__btn {
+      background: none;
+      border: 2px solid black;
+      padding: 5px 20px;
+    }
+  </style>
+  <button class="arrows__btn arrows__btn--prev" type="button"><-</button>
+  <button class="arrows__btn arrows__btn--next" type="button">-></button>`;
 
-    let arrows = arrowsDiv.querySelectorAll(`.arrows__btn`);
-    arrows[0].addEventListener(`click`, (evt) => {
-      evt.preventDefault();
+  arrowsDiv.addEventListener(`click`, (evt) => {
+    const targetClass = evt.target.classList;
+    if (targetClass.contains(`arrows__btn--prev`)) {
       setScreen(screenCurrent - 1);
-    });
-    arrows[1].addEventListener(`click`, (evt) => {
-      evt.preventDefault();
+      return;
+    }
+    if (targetClass.contains(`arrows__btn--next`)) {
       setScreen(screenCurrent + 1);
-    });
+      return;
+    }
+  });
 
-    app.appendChild(arrowsDiv);
-  }
-  renderArrows();
+  app.appendChild(arrowsDiv);
 }
+renderArrows();
